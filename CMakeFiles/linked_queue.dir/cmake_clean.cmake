684192ecc827501a646a688c47872c5a5e3f44e1
FroggyElder <shadowskinC@gmail.com>
2024-06-12 20:18:36 +0800

    www

---
+++ CMakeFiles/linked_queue.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/linked_queue.dir/linked_queue.c.o"
+  "CMakeFiles/linked_queue.dir/linked_queue.c.o.d"
+  "liblinked_queue.a"
+  "liblinked_queue.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/linked_queue.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
